876c11c26e5c38352e483781dacd0ab65cab6997
martio13 <martin@othamar.net>
2013-09-12 17:22:23 +0200

    wtf

--- bygg.php
+++ bygg.php
@@ -4,23 +4,11 @@
 <!-- class er .  id er # -->
 <link rel="stylesheet" type="text/css" href="teststyle.css">
 <title>PC Byggaren</title>
-<<<<<<< HEAD
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-</head>
-
-
-<<<<<<< HEAD
-<!-- Hovedfunksjonalitet på webapplikasjonen. Åpnes i ny fane via index.php. 
-=======
-<!-- Hovedfunksjonalitet pÂ webapplikasjonen. ≈pnes i ny fane via index.php. 
->>>>>>> 27eb910176cd7215799a03f72fa3d89fc7aee973
-=======
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 
 
 <!-- Hovedfunksjonalitet p√• webapplikasjonen. √•pnes i ny fane via index.php. 
->>>>>>> 957d16abf71ff3c04118bdb27912986f990f4bbf
 Kopi av meny og struktur ref index.php + intro. Funksjonalitet kommer -->
 
 
@@ -32,31 +20,9 @@ Kopi av meny og struktur ref index.php + intro. Funksjonalitet kommer -->
 <div class="formwindow">
 	
 	<h3 class="byggtekst">Bygg din PC<br></h3>
-<<<<<<< HEAD
-<<<<<<< HEAD
-	<p class="byggtekst">Kort introduksjon til hvordan prosessen foregår og veilending <br>til hvordan oppnå
-	best mulig resultat med PC-byggingen.</p><br>
-	
-	<p class="byggtekst">Her kommer stort ikon for 'BEGYNN Å BYGGE'</p><br><br>
-	
-	<h4 class="byggtekst">Forms som dukker opp når ikon blir klikket/evt. ny side</h4>
-	<li class="byggtekst">liste</li>
-	<li class="byggtekst">liste</li><br><br>
-
-	<h4 class="byggtekst">Forms</h4>
-	<li class="byggtekst">liste</li>
-	<li class="byggtekst">liste</li>
-	<li class="byggtekst">liste</li>
-=======
-	<p class="byggtekst">Her begynner byggefunksjonen. Start byggingen ved Â trykke pÂ ikonet!</p><br><br>
-	<a href="byggstart.php"><img src="bygg.jpg" class="byggikon" alt="byggikon" width="300" height="100"></a>	
-
->>>>>>> 27eb910176cd7215799a03f72fa3d89fc7aee973
-=======
 	<p class="byggtekst">Her begynner byggefunksjonen. Start byggingen ved √• trykke p√• ikonet!</p><br><br>
 	<a href="byggstart.php"><img src="bygg.png" class="byggikon" alt="byggikon" width="300" height="100"></a>	
 
->>>>>>> 957d16abf71ff3c04118bdb27912986f990f4bbf
 </div>
 
 
