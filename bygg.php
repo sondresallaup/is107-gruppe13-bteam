957d16abf71ff3c04118bdb27912986f990f4bbf
martio13 <martin@othamar.net>
2013-09-12 17:11:41 +0200

    Byggeikon i bygg.php

--- bygg.php
+++ bygg.php
@@ -1,14 +1,14 @@
-<!DOCTYPE html>
+ï»¿<!DOCTYPE html>
 <html>
 <head>
 <!-- class er .  id er # -->
 <link rel="stylesheet" type="text/css" href="teststyle.css">
 <title>PC Byggaren</title>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 
 
-<!-- Hovedfunksjonalitet på webapplikasjonen. Åpnes i ny fane via index.php. 
+<!-- Hovedfunksjonalitet pÃ¥ webapplikasjonen. Ã¥pnes i ny fane via index.php. 
 Kopi av meny og struktur ref index.php + intro. Funksjonalitet kommer -->
 
 
@@ -20,7 +20,7 @@ Kopi av meny og struktur ref index.php + intro. Funksjonalitet kommer -->
 <div class="formwindow">
 	
 	<h3 class="byggtekst">Bygg din PC<br></h3>
-	<p class="byggtekst">Her begynner byggefunksjonen. Start byggingen ved å trykke på ikonet!</p><br><br>
+	<p class="byggtekst">Her begynner byggefunksjonen. Start byggingen ved Ã¥ trykke pÃ¥ ikonet!</p><br><br>
 	<a href="byggstart.php"><img src="bygg.jpg" class="byggikon" alt="byggikon" width="300" height="100"></a>	
 
 </div>
