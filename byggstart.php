d22571213d097aa306e14cb5ebb4a5102f0e86be
martio13 <martin@othamar.net>
2013-09-12 17:50:58 +0200

    Påbegynt byggstart.php
    Her skal ikoner for brukerprofiler settes inn.

---
+++ byggstart.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- class er .  id er # -->
+<link rel="stylesheet" type="text/css" href="teststyle.css">
+<title>PC Byggaren</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+
+<body>
+<div id="content">
+
+<h1>Start på veiledning</h1><br>
+<p>Vennligst velg den brukerprofilen som passer deg best. </p>
+
+
+
+<?php
+
+
+
+?>
+
+
+
+
+
+
+</div>
