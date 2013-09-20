819a6c682b79a2c2c710b2f0c66538100d4c71a7
stiank13 <s.killerud@gmail.com>
2013-09-20 18:19:13 +0200

    Lagt til footer og formatering
    La til footer og fikset på litt formatering

--- index.php
+++ index.php
@@ -1,102 +1,99 @@
 <!DOCTYPE html>
 <html>
 <head>
-<!-- class er .  id er # -->
-<LINK REL="SHORTCUT ICON" HREF="/favicon.ico" type="image/x-icon">
-<link rel="stylesheet" type="text/css" href="style.css">
-<title>PC Byggaren</title>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<!-- class er .  id er # -->
+	<LINK REL="SHORTCUT ICON" HREF="/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>PC Byggaren</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 
 <?php
-include_once "mysql_connect.php"
+	include_once "mysql_connect.php"
 ?> 
 
 
-<!-- Hovedindex for webapplikasjon. -->
+	<!-- Hovedindex for webapplikasjon. -->
+	
 
+	<!-- Funksjon for endring av source i iframe. -->
+	<script>
+		function updateProfil(){
+			document.getElementById("iframewindow").src="minprofil.php"
+		}
 
-<!-- Funksjon for endring av source i iframe. -->
-<script>
-function updateProfil(){
-	document.getElementById("iframewindow").src="minprofil.php"
-}
+		function updateProsjekter(){
+			document.getElementById("iframewindow").src="prosjekter.php"
+		}
 
-function updateProsjekter(){
-	document.getElementById("iframewindow").src="prosjekter.php"
-}
+		function updateKontakt(){
+			document.getElementById("iframewindow").src="kontakt.php"
+		}
 
-function updateKontakt(){
-	document.getElementById("iframewindow").src="kontakt.php"
-}
-
-function updateBygg(){
-	document.getElementById("iframewindow").src="bygg.php"
-}
-</script>
+		function updateBygg(){
+			document.getElementById("iframewindow").src="bygg.php"
+		}
+	</script>
 
 
 <body>
 <div id="content">
 
 
-<!-- Tittel. -->
-<div class="header">	
-	<h1><a href="http://kongeprosjekter.net/"><abbr title="Forside">PC Byggaren</abbr></a></h1>
-</div>
+	<!-- Tittel. -->
+	<div class="header">	
+		<h1><a href="http://kongeprosjekter.net/"><abbr title="Forside">PC Byggaren</abbr></a></h1>
+	</div>
 
 
-<!-- Topp-menyen. Klikk menyvalg = execute funksjon for endring av iframe-source ref linje 15-27. -->
-<div class="menutop">
-	<ul>
-	<li class="top"><a class="menu" href="#" onclick="updateProfil()"><abbr title="Min Profil">Min Profil</abbr></a></li>
-	<li class="top"><a class="menu" href="#" onclick="updateProsjekter()"><abbr title="Prosjekter">Prosjekter</abbr></a></li>
-	<li class="top"><a class="menu" href="#" onclick="updateKontakt()"><abbr title="Kontakt">Kontakt</abbr></a></li>
-	<li class="top"><a class="menu" href="#" onclick="updateBygg()""><abbr title="Bygg">Bygg</abbr></a></li>
-</ul>
-</div>
+	<!-- Topp-menyen. Klikk menyvalg = execute funksjon for endring av iframe-source ref linje 15-27. -->
+	<div class="menutop">
+		<ul>
+			<li class="top"><a class="menu" href="#" onclick="updateProfil()"><abbr title="Min Profil">Min Profil</abbr></a></li>
+			<li class="top"><a class="menu" href="#" onclick="updateProsjekter()"><abbr title="Prosjekter">Prosjekter</abbr></a></li>
+			<li class="top"><a class="menu" href="#" onclick="updateKontakt()"><abbr title="Kontakt">Kontakt</abbr></a></li>
+			<li class="top"><a class="menu" href="#" onclick="updateBygg()""><abbr title="Bygg">Bygg</abbr></a></li>
+		</ul>
+	</div>
 
 
-<!-- Hovedvinduet embedded via iframe. -->
-<iframe id="iframewindow" scrolling="no" src="forside.php" width="790"; height="600";></iframe>
-
-
-<!-- GAMLE INFO VINDU! IKKE I BRUK!
-<div class="infowindow">
+	<!-- Hovedvinduet embedded via iframe. -->
+	<iframe id="iframewindow" scrolling="no" src="forside.php" width="790"; height="600";></iframe>
 	
-	<p><img src="meg2.jpg" alt="Stian" border="1" height="151" width="214" align="top">Her kommer det tekst. <br><br><br>
+	<!-- div mainwindow som kan brukes når vi fjerner iFrame.. Stylet og kommentert ut i css
+	<div id="mainwindow">
+		<p>hovedvindu</p>
+	</div>
+	-->
 
 	
-	<h4>Utdanning</h4>
-	<li>liste</li>
-	<li>liste</li><br><br>
-
-	<h4>Jobb</h4>
-	<li>liste</li>
-	<li>liste</li>
-	<li>liste</li>
-</div>-->
-
-
-<!-- Sidemeny, ingen funksjon ennÃ¥. -->
-<div class="sidebar">
-	<form action="login.php" method="POST">
-	<ul>
-	<li><br>Brukernavn: <input type="text" name="username">
-	<li>Passord: <input type="password" name="password">
-	<li><input type="submit" value="Logg inn">
+	<!-- Sidemeny, ingen funksjon ennÃ¥. -->
+	<div class="sidebar">
+		<form action="login.php" method="POST">
+		<ul>
+			<li><br>Brukernavn: <input type="text" name="username">
+			<li>Passord: <input type="password" name="password">
+			<li><input type="submit" value="Logg inn">
 	
-	<li><br>Når logget inn:</li>
-	<li>Mine PC'er</li>
-	<li>Mitt galleri</li>
-	<li>Mine innlegg</li>
-	<li>Instillinger</li><br>
-	<li>Upload</li>
-	<li>Forum</li><br><br>
-	<li>Logg ut</li>
-	</ul>
-	</form>
-</div>
+			<li><br>Når logget inn:</li>
+			<li>Mine PC'er</li>
+			<li>Mitt galleri</li>
+			<li>Mine innlegg</li>
+			<li>Instillinger</li><br>
+			<li>Upload</li>
+			<li>Forum</li><br><br>
+			<li>Logg ut</li>
+		</ul>
+		</form>
+	</div>
+
+
+	<div id="clear"></div>
+	
+	<div id="footer">
+		<p>Footer</p>
+	</div>
+
 
 
 </div>
