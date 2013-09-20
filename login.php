eedbb197292db1b3088537248459bd7cc094a804
sondresallaup <sondresallaup@Sondres-MacBook-Pro.local>
2013-09-20 17:01:44 +0200

    Noe testing av login.php

--- login.php
+++ login.php
@@ -1,7 +1,10 @@
 <?php
 
-$username = $_POST["username"];
-$password = $_POST["password"];
+
+@$username = $_POST['username'];
+@$password = $_POST['password'];
+
+
 
 if($username&&$password)
 {
@@ -9,11 +12,18 @@ if($username&&$password)
 $connect = mysql_connect("mysql23int.stwadmin.net", "u1010446_root","Bteam2013" or die("Kan ikke koble til");
 mysql_select_db("db1010446_pcbyggaren") or die("Finner ikke db");
 
+$query = mysql_query("SELECT * FROM brukere WHERE brukernavn='$username'");
+
+$numrows = mysql_num_rows($query);
+
+echo $numrows;
 
 
 }
 
-else
+else{
 	die("Mangler brukernavn og passord");
+	
 
+}
 ?>
