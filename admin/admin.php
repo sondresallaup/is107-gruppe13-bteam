ae8849621a3783fd0e55386e9d6cc1b46f3b27e2
kennethfinnesand <kennethfinnesand@gmail.com>
2013-09-23 16:19:52 +0200

    La til vegg på adminside
    nå kan folk skrive meldinger til andre admins

--- admin/admin.php
+++ admin/admin.php
@@ -1,9 +1,36 @@
 <html>
 <head>
-<?php session_start(); ?>
+<?php //session_start(); ?>
+<?php include "mysql_connect.php" ?>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <body>
-Her skal det være noe som ligner på en admin hjemmeside
+<div class="meldingvegg">
+
+<form action="adminvegg.php" method="post" >
+<textarea name="test" rows="5" cols="50" required maxlength="255"></textarea><br>
+<input type="submit" value="post" /><br><br>
+<div class="scrollbar" style="height:460px;;border:0px solid #ccc;font:16px/26px Georgia, Garamond, Serif;overflow:auto;">
+
+<?php
+
+
+//hente alt fra tabell
+$tabell = "adminvegg";
+$hent = "SELECT * FROM $tabell";
+$liste = mysql_query($hent);
+
+//skriver ut tabellen
+while($rad = mysql_fetch_assoc($liste)) {
+
+echo $rad['tid']; echo "<br>";
+echo $rad['melding']; echo "<br><br>";
+
+}
+?>
+</div>
+
+</form>
+</div>
 </body>
 </html>
