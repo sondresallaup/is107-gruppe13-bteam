71c2a9c34680d8b02c60741dc8f68fbbf4da0d0a
stiank13 <s.killerud@gmail.com>
2013-09-23 19:04:41 +0200

    La til enkel form på opprettprosjekt
    Enkel førsteutkast form inntil videre

--- opprettprosjekt.php
+++ opprettprosjekt.php
@@ -24,10 +24,61 @@ include_once "mysql_connect.php"
 
 <body>
 	<div id="content">
-	<h1>test</h1>
+	<h1>Opprett et prosjekt</h1>
 
-
+	<form action="#" method="get" id="prosjektform">
+		<input type="text" name="prosjektnavn" placeholder="Prosjektnavn" />
+		<select name="prosjektstatus">
+			<option value="prosjektferdig">Ferdig</option>
+			<option value="prosjektwip" selected>Work In Progress</option>
+			<option value="prosjektide">Idé</option>
+		</select>
+		<!-- knapper i form av <a> for å legge til / fjerne felt, styles senere -->
+		<a href="#" onclick="leggTilKomponent()">Ny komponent!</a>
+		<a href="#" onclick="fjernKomponent()">Fjern komponent!</a>
+		<input type="text" name="prosjektkomponent" placeholder="Komponenter" />
+		<input type="text" name="prosjektkomponent" placeholder="Komponenter" /> 
+		<input type="text" name="prosjektkomponent" placeholder="Komponenter" /> 
+		<input type="text" name="prosjektkomponent" placeholder="Komponenter" /> 
+		
+		<textarea name="Name" rows="10" cols="40" placeholder="Skriv litt om prosjektet" />
+		
+		<input type="submit" name="prosjektsubmit">Opprett Prosjekt</input>
+		
+	</form>
+	<span id="nochildspan" style="display:none";></span>
 
 </div>
+<script> //funksjoner for å legge til og fjerne input felt under komponenter i formen..
+	function leggTilKomponent(){
+		var nyttprosjektform=document.getElementById("prosjektform");
+		var nochild=document.getElementById("nochildspan");
+		var nyinput=document.createElement("input");
+		
+		nyinput.setAttribute("type","text");
+		nyinput.setAttribute("name","nyinputelem");
+		nyinput.setAttribute("placeholder","Komponent");
+		nyinput.style.display: "block";
+		nochild.style.display: "none";
+		
+		nyttprosjektform.appendChild(nyinput);
+	}
+	
+	function fjernKomponent(); {
+		var nyttprosjektform=document.getElementById("prosjektform");
+		var nochild=document.getElementById("nochildspan");
+		var lastchild=nyttprosjektform.lastChild;
+		
+		if (lastchild){
+			nyttprosjektform.removeChild(lastchild);
+			nochild.style.display: "none";
+		} else {
+			nochild.style.display: "block";
+			nochild.innerHTML: "Ingen flere felt å fjerne";
+		}
+	}
+	
+</script>
+
 </body>
 </html>
