09889bd8e3c6ba0e592ef34791d40d5949ef2622
Sondre Sallaup <sondre.sallaup@gmail.com>
2013-09-23 19:34:29 +0200

    Revert "Merge branch 'master' of https://github.com/kennethfinnesand/is107-gruppe13-bteam"
    This reverts commit c060034b1202052effaaa86783dd18bd80f4fc11, reversing
    changes made to 0df172d83914ccdbdb09a95f465491b72e51f70f.

--- admin/adminvegg.php
+++ admin/adminvegg.php
@@ -5,20 +5,12 @@ content="0; URL=admin.php">
 </head>
 <body>
 <?php
-<<<<<<< HEAD
 session_start();
 include "mysql_connect.php";
 $meldinger = $_POST['test'];
 $bruker = $_SESSION['username'];
 
 mysql_query("INSERT INTO 'adminvegg' ('melding','bruker') VALUES ('$meldinger','$bruker')");
-=======
-include "mysql_connect.php";
-$meldinger = $_POST['test'];
-
-
-mysql_query("INSERT INTO 'adminvegg' ('melding') VALUES ('$meldinger')");
->>>>>>> ae8849621a3783fd0e55386e9d6cc1b46f3b27e2
 
 ?>
 <?php 
@@ -46,11 +38,7 @@ $liste = mysql_query($hent);
 //skriver ut tabellen
 while($rad = mysql_fetch_assoc($liste)) {
 
-<<<<<<< HEAD
 echo $rad['tid']; echo " "; echo $rad['bruker']; echo "<br>";
-=======
-echo $rad['tid']; echo "<br>";
->>>>>>> ae8849621a3783fd0e55386e9d6cc1b46f3b27e2
 echo $rad['melding']; echo "<br><br>";
 
 }
