5bdca1f2f00dcf3d0c7149d5583659ee58d90001
Sondre Sallaup <sondre.sallaup@gmail.com>
2013-09-25 12:50:33 +0200

    Info knyttet til søk

--- index.php
+++ index.php
@@ -109,6 +109,7 @@ session_start();
 		<ul>
 		<!-- Search-input -->
 		<form method="POST" action="search.php" method="POST">
+			<li>Søk etter bruker (brukernavn kun støttet til nå)</li>
 			<li><br><input type="text" name="search"></li>
 			<li><input type="submit" value="søk"></li>
 		</form>
